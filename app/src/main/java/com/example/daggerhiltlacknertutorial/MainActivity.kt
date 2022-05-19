ec3ab471acb3e4dba51a2d828cf0fbfd5cc90b64
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2022-05-19 14:33:31 -0500

    Created MainModule which uses dependency from String 1 in AppModule

--- app/src/main/java/com/example/daggerhiltlacknertutorial/MainActivity.kt
+++ app/src/main/java/com/example/daggerhiltlacknertutorial/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.daggerhiltlacknertutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import javax.inject.Named
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    @Named("String2")
+    lateinit var testString : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "Test string from MainActivity: $testString")
     }
 }
